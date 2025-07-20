6a77023bd5a8440279aab13504c2298d80c14ced
chris-simplified <165951138+chris-simplified@users.noreply.github.com>
2025-07-20 10:19:19 +0100

    update

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -17,7 +17,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: process.env.API_URL || 'http://localhost:3000',
+        url: process.env.API_URL || 'http://localhost:3300',
         description: 'Development server',
       },
     ],
